70072b37949432f18725c0a9aec21d49c3fb1d66
Tammy Richardson <atrich0220@gmail.com>
2020-02-28 11:07:23 -0700

    added questions and answers array

---
+++ assets/javascript/app.js
@@ -0,0 +1,140 @@
+var questions = [
+    {   //1
+        question: "What is the name of the man Rachel almost marries?",
+        answers: {
+            a: "Alan",
+            b: "Chris",
+            c: "Billy",
+            d: "Barry"
+        },
+        correctAnswer: "d"
+    },
+    {   //2
+        question: "What is Ross' first wife's name?",
+        answers: {
+            a: "Emily",
+            b: "Susan",
+            c: "Carol",
+            d: "Heather"
+        },
+        correctAnswer: "c"
+    },
+    {   //3
+        question: "Which soap opera is Joey on?",
+        answers: {
+            a: "Days of Our Lives",
+            b: "All My Children",
+            c: "General Hospital",
+            d: "As the World Turns"
+        },
+        correctAnswer: "a"
+    },
+    {   //4
+        question: "What is the name of Ross and Rachel's daughter?",
+        answers: {
+            a: "Julie",
+            b: "Emma",
+            c: "Katie",
+            d: "Sarah"
+        },
+        correctAnswer: "b"
+    },
+    {   //5
+        question: "What is the name of the Central Perk employee who's in love with Rachel?",
+        answers: {
+            a: "George",
+            b: "Gunther",
+            c: "Gary",
+            d: "Geoffry"
+        },
+        correctAnswer: "b"
+    },
+    {   //6
+        question: "how many sisters does Joey have?",
+        answers: {
+            a: "Seven",
+            b: "Five",
+            c: "Six",
+            d: "Eight"
+        },
+        correctAnswer: "a"
+    },
+    {   //7
+        question: "What doe Phoebe do for a living?",
+        answers: {
+            a: "Chef",
+            b: "Teacher",
+            c: "Massuuse",
+            d: "Nurse"
+        },
+        correctAnswer: "c"
+    },
+    {   //8
+        question: "Where does Rachel almost move to at the end of the series?",
+        answers: {
+            a: "Milan",
+            b: "London",
+            c: "Rome",
+            d: "Paris"
+        },
+        correctAnswer: "d"
+    },
+    {   //9
+        question: "What do they call the neighbor across the street?",
+        answers: {
+            a: "The Nudist",
+            b: "Ugly Naked Guy",
+            c: "Naked Guy",
+            d: "Ugly Guy"
+        },
+        correctAnswer: "b"
+    },
+    {   //10
+        question: "Where does Chandler transfer to for his job?",
+        answers: {
+            a: "Tulsa",
+            b: "Topeka",
+            c: "Des Moines",
+            d: "Boise"
+        },
+        correctAnswer: "a"
+    },
+    {   //11
+        question: "Who finds out about Monica and Chandler first?",
+        answers: {
+            a: "Pheobe",
+            b: "Ross",
+            c: "Joey",
+            d: "Rachel"
+        },
+        correctAnswer: "c"
+    },
+    {   //12
+        question: "What is the name of the eye doctor Monica dates?",
+        answers: {
+            a: "Ben",
+            b: "William",
+            c: "Richard",
+            d: "Robert"
+        },
+        correctAnswer: "c"
+    }
+];
+
+
+var answers
+var correct
+var incorrect
+var timer
+var seconds = 0;
+var interval;
+
+function buildQuiz()
+
+function showResults()
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
