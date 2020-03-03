84d70f6826dcb2702755802cf5f9e045ddb4683d
Tammy Richardson <atrich0220@gmail.com>
2020-03-03 12:01:20 -0700

    changed to easier assignment, input questions and answers into form with inline radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var question = [
     {   //1
-        question: "What is the name of the man Rachel almost marries?",
+        prompt: "What is the name of the man Rachel almost marries?",
         answers: {
             a: "Alan",
             b: "Chris",
@@ -10,7 +10,7 @@ var question = [
         correctAnswer: "d"
     },
     {   //2
-        question: "What is Ross' first wife's name?",
+        prompt: "What is Ross' first wife's name?",
         answers: {
             a: "Emily",
             b: "Susan",
@@ -20,7 +20,7 @@ var question = [
         correctAnswer: "c"
     },
     {   //3
-        question: "Which soap opera is Joey on?",
+        prompt: "Which soap opera is Joey on?",
         answers: {
             a: "Days of Our Lives",
             b: "All My Children",
@@ -30,7 +30,7 @@ var question = [
         correctAnswer: "a"
     },
     {   //4
-        question: "What is the name of Ross and Rachel's daughter?",
+        prompt: "What is the name of Ross and Rachel's daughter?",
         answers: {
             a: "Julie",
             b: "Emma",
@@ -40,7 +40,7 @@ var question = [
         correctAnswer: "b"
     },
     {   //5
-        question: "What is the name of the Central Perk employee who's in love with Rachel?",
+        prompt: "What is the name of the Central Perk employee who's in love with Rachel?",
         answers: {
             a: "George",
             b: "Gunther",
@@ -50,7 +50,7 @@ var question = [
         correctAnswer: "b"
     },
     {   //6
-        question: "how many sisters does Joey have?",
+        prompt: "how many sisters does Joey have?",
         answers: {
             a: "Seven",
             b: "Five",
@@ -60,7 +60,7 @@ var question = [
         correctAnswer: "a"
     },
     {   //7
-        question: "What doe Phoebe do for a living?",
+        prompt: "What doe Phoebe do for a living?",
         answers: {
             a: "Chef",
             b: "Teacher",
@@ -70,7 +70,7 @@ var question = [
         correctAnswer: "c"
     },
     {   //8
-        question: "Where does Rachel almost move to at the end of the series?",
+        prompt: "Where does Rachel almost move to at the end of the series?",
         answers: {
             a: "Milan",
             b: "London",
@@ -80,7 +80,7 @@ var question = [
         correctAnswer: "d"
     },
     {   //9
-        question: "What do they call the neighbor across the street?",
+        prompt: "What do they call the neighbor across the street?",
         answers: {
             a: "The Nudist",
             b: "Ugly Naked Guy",
@@ -90,7 +90,7 @@ var question = [
         correctAnswer: "b"
     },
     {   //10
-        question: "Where does Chandler transfer to for his job?",
+        prompt: "Where does Chandler transfer to for his job?",
         answers: {
             a: "Tulsa",
             b: "Topeka",
@@ -100,7 +100,7 @@ var question = [
         correctAnswer: "a"
     },
     {   //11
-        question: "Who finds out about Monica and Chandler first?",
+        prompt: "Who finds out about Monica and Chandler first?",
         answers: {
             a: "Pheobe",
             b: "Ross",
@@ -110,7 +110,7 @@ var question = [
         correctAnswer: "c"
     },
     {   //12
-        question: "What is the name of the eye doctor Monica dates?",
+        prompt: "What is the name of the eye doctor Monica dates?",
         answers: {
             a: "Ben",
             b: "William",
@@ -123,122 +123,99 @@ var question = [
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var currentQuestion
-//var shuffledQuestion
-var questionElement = document.getElementById("question")
-var questionElement = document.getElementById("choice-buttons")
+//assigns question to html id
+var questionElement = $("#question")
+//assigns answers to html button id
+var answerElement = $("#choice-buttons")
 
 //start the game
 var startButton = document.getElementById("start-btn")
 startButton.addEventListener("click", startQuiz)
+//when a selection is made
+var userChoice = document.getElementById("#choice-buttons")
+userChoice.addEventListener("click", selection)
+
+
 var questionContainer = document.getElementById
     ("question-container")
 
 function startQuiz() {
-    console.log("started");
-    //this will unhide the question and answer button when start button is clicked
-    questionContainer.classList.remove("hide");
-    //this will shuffle the questions
-    //shuffledQuestions = question.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    //displays question in question div on card
-    $("#question").text(question);
-    //new question
-    newQuestion();
-    reset();
-}
-
-function newQuestion() {
-    showQuestion(question[currentQuestionIndex])
-
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-}
-
-function selectAnswer() {
-
-}
-
-
-//timer
-var counter = 30
-
-var myTimer = setInterval(function () {
-    //we want to count down from 30 to 0 each second 
-    //use counter variable which decrements every second used to update the html
-    counter--
-    console.log(counter)
-    //use jQuery to update HTML
-    //grab location from HTML and the use .text to diplay counter variable
-    $("#time").text(counter)
-    //to get the interval to start over again
-    if (counter == 0) {
-        //reset()
-        clearInterval(myTimer)
+    //loop through questions array
+    for (var i = 0; i < question.length; i++) {
+        //displays question in question div on card
+        $("#question").text(question[i].prompt);
+        //this will display the answer choices on buttons
+        $(".btnA").html(question[i].answers.a)
+        $(".btnB").html(question[i].answers.b)
+        $(".btnC").html(question[i].answers.c)
+        $(".btnD").html(question[i].answers.d)
+        reset()
     }
-}, 1000);
 
-function reset() {
-    //calling myTimer to reset the interval and the counter
-    myTimer
-    counter = 30
+}
+function selection() {
+    //if the button clicked = the correct answer generate new question and update correct answer tally
+    //$(".btn").click(function () {
+    //console.log("You clicked on: " + $(this).val());
+    //console.log("The correct answer was: " + question[i].correctAnswer);
+    if (userChoice == question[i].correctAnswer) {
+        rightAnswer();
+        question[i]++
+        //if the button clicked = any other answer generate new question and update wrong answer tally  
+    } else {
+        wrongAnswer()
+        question[i]++
+    }
 }
 
-//display questions
-//i want a question to appear
-//i want a new question to appear when an answer button is clicked
-function newQuestion() {
-    //store the total number of questions
-    var totalQuestions = $("question.length");
-    //set the current question to display to 1
-    var currentQuestions = 0;
-    //store the selector in a variable
-    $question = $("question");
-    //hide all the questions
-    $question.hide();
-    //attach a click listener to the html element with the id of next
-    $("next").click(function () {
-        //fade out the current question,
-        //putting a function inside of fadeOut calls that function 
-        //immediately after fadeOut is completed, 
-        //this is for a smoother transition animation
-        $($questions.get(currentQuestion)).fadeOut(function () {
 
-            //increment the current question by one
-            currentQuestion = currentQuestion + 1;
-
-            //if there are no more questions do stuff
-            if (currentQuestion == totalQuestions) {
-
-                var result = sum_values()
-
-                //do stuff with the result
-                alert(result);
-
-            } else {
-
-                //otherwise show the next question
-                $($questions.get(currentQuestion)).fadeIn();
-
-            }
-        });
-
-
-    });
-
-    function rightAnswer() {
-        correctAnswers++;
+//if answered correctly
+function rightAnswer() {
+    if (selected === correctAnswer) {
+        alert("correct")
         $("#correct").text(correctAnswers);
-
+        reset()
+        correctAnswers++;
     }
-
+    //if answred incorrectly
     function wrongAnswer() {
-        incorrectAnswers++;
-        $("#incorrect").text(incorrectAnswers);
+        if (selected == answer) {
+            alert("incorrect")
+            $("#incorrect").text(incorrectAnswers);
+            reset()
+            incorrectAnswers++;
+        }
 
+
+        //timer
+        var counter = 30
+
+        var myTimer = setInterval(function () {
+            //we want to count down from 30 to 0 each second 
+            //use counter variable which decrements every second used to update the html
+            counter--
+            console.log(counter)
+            //use jQuery to update HTML
+            //grab location from HTML and the use .text to diplay counter variable
+            $("#time").text(counter)
+            //to get the interval to start over again
+            if (counter == 0) {
+                //reset()
+                clearInterval(myTimer)
+            }
+        }, 1000);
+
+        function reset() {
+            //calling myTimer to reset the interval and the counter
+            myTimer
+            counter = 30
+
+        }
     }
+
+
 }
+
+
 
 
